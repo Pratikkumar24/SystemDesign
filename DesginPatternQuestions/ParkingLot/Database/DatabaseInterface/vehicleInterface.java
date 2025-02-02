5a838c26adb641cc7136810db6b2b7f6bdc18342
PratikKumar24 <pratikkumar200024@gmail.com>
2025-02-02 12:26:07 +0530

    Restarting my design patterns

--- DesginPatternQuestions/ParkingLot/Database/DatabaseInterface/vehicleInterface.java
+++
@@ -1,7 +0,0 @@
-package Database.DatabaseInterface;
-
-public interface vehicleInterface {
-    boolean isVehicleIdPresent(int id);
-    void removeVehicleId(int id);
-    void addVehicleId(int id);
-}
--- DesginPatternQuestions/ParkingLot/Utility/IDGenerator.java
+++
@@ -1,8 +0,0 @@
-package Utility;
-
-import java.util.Random;
-
-public interface IDGenerator {
-    Random random = new Random();
-    int generateId();
-}
