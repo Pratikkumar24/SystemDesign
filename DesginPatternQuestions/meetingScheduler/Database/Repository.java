32272114628cf24225f4630a87717579b4848a81
PratikKumar24 <pratikkumar200024@gmail.com>
2025-02-07 20:21:48 +0530

    added meeting room arraylist

--- DesginPatternQuestions/meetingScheduler/Database/Repository.java
+++ DesginPatternQuestions/meetingScheduler/Database/Repository.java
@@ -2,6 +2,8 @@ package SystemDesign.DesginPatternQuestions.meetingScheduler.Database;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
 
 import SystemDesign.DesginPatternQuestions.meetingScheduler.Entity.MeetingRoom;
 import SystemDesign.DesginPatternQuestions.meetingScheduler.Person.User;
@@ -9,12 +11,15 @@ import SystemDesign.DesginPatternQuestions.meetingScheduler.Person.User;
 public class Repository {
     HashMap<int[], ArrayList<MeetingRoom>> meetingRooms;
     HashMap<String, User> emailToUsers;
-
+    ArrayList<MeetingRoom> meetingRoomSet;
+    Random random;
     public static Repository instances;
 
     private Repository() {
         meetingRooms = new HashMap<>();
         emailToUsers = new HashMap<>();
+        meetingRoomSet = new ArrayList<>();
+        random = new Random();
     }
 
     public static Repository getInstance() {
@@ -40,5 +45,45 @@ public class Repository {
 
     public User getUserByEmail(String email) {
         return emailToUsers.get(email);
+    }
+
+    public void updateMeetingRoom(MeetingRoom meetingRoom) {
+        meetingRoomSet.add(meetingRoom);
+    }
+
+    public boolean deleteMeetingRoom(int id) {
+        for(MeetingRoom meetingRoom: meetingRoomSet) {
+            if(meetingRoom.getMeetingId() == id) {
+                meetingRoomSet.remove(meetingRoom);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer getMeetingRoom(HashSet<Integer> bookedMeetingRoomIds) {
+        for(MeetingRoom meetingRoom: meetingRoomSet) {
+            if(!bookedMeetingRoomIds.contains(meetingRoom.getMeetingId())) {
+                return meetingRoom.getMeetingId();
+            }
+        }
+        return -1;   
+    }
+
+    public int generateId() {
+        int id = 0;
+        do {
+            id = random.nextInt(10);
+        } while(!isMeetingIdPresent(id));
+        return id;
+    }
+
+    public boolean isMeetingIdPresent(int id) {
+        for(MeetingRoom meetingRoom: meetingRoomSet) {
+            if(meetingRoom.getMeetingId() == id) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- DesginPatternQuestions/meetingScheduler/Person/AdminService.java
+++ DesginPatternQuestions/meetingScheduler/Person/AdminService.java
@@ -1,8 +1,9 @@
 package SystemDesign.DesginPatternQuestions.meetingScheduler.Person;
 
 import SystemDesign.DesginPatternQuestions.meetingScheduler.Database.Repository;
+import SystemDesign.DesginPatternQuestions.meetingScheduler.Entity.MeetingRoom;
 
-public class AdminService{
+public class AdminService {
 
     private static Repository repository = Repository.getInstance();
     private static AdminService adminService;
@@ -19,8 +20,24 @@ public class AdminService{
         }
         return adminService;
     }
+    
     public void createUser(String email, int userId) {
         User client = new Client(email, userId);
         repository.updatedEmailToUsers(email, client);
+    }
+
+    public void createMeetingRoom(String roomName, int capacity) {
+        int id = repository.generateId();
+        MeetingRoom meetingRoom = new MeetingRoom(id, capacity, 0,0);
+        repository.updateMeetingRoom(meetingRoom);
+    }
+
+    public void deleteMeetingRoom(int id) {
+       boolean isDeleted = repository.deleteMeetingRoom(id);
+        if(!isDeleted) {
+            System.out.println("Meeting room not found");
+        } else {
+            System.out.println("Meeting room deleted successfully");
+        }
     }
 }
